aa89622c28e934322fea28ae63cdc4ce3bda007b
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-04 14:27:30 +0100

    program now changes 3 to fizz and 5 to buzz and updated spec to cover these conditions

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  elsif 3 && 5
+    "fizzbuzz"
+  elsif !number.is_a? Integer
+    "Error - not an integer"
+  else
+    return number
+  end
 end
