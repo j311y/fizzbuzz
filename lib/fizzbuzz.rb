b202aa12c15701a423509ee816dddce25181f7d0
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-04 15:34:50 +0100

    working fizzbuzz code kata with normal and edge case testing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,24 @@
+# def fizzbuzz(number)
+#   if !number.is_a? Integer
+#     "Error - not an integer"
+#   elsif number <= 0
+#     "Please use a positive integer"
+#   elsif number % 15 == 0
+#     "fizzbuzz"
+#   elsif number % 5 == 0
+#     "buzz"
+#   elsif number % 3 == 0
+#     "fizz"
+#   else
+#     number
+#   end
+# end
+
 def fizzbuzz(number)
-  if number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  elsif 3 && 5
-    "fizzbuzz"
-  elsif !number.is_a? Integer
-    "Error - not an integer"
-  else
-    return number
-  end
+  return "Error - not an integer" if !number.is_a? Integer
+  return "Please use a positive integer" if number <= 0
+  return "fizzbuzz" if number % 15 == 0
+  return "buzz" if number % 5 == 0
+  return "fizz" if number % 3 == 0
+  number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,23 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when passed 3 or 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+   it 'returns "Error - not an integer" when an integer is not passed' do
+     expect(fizzbuzz("string")).to eq "Error - not an integer"
+   end
+
+   it 'returns "Error - not an integer" when an integer is not passed' do
+     expect(fizzbuzz(3.1)).to eq "Error - not an integer"
+   end
+
+   it 'returns "Please use a positive integer" when a negative integer is passed' do
+     expect(fizzbuzz(-3)).to eq "Please use a positive integer"
+   end
+
+   it 'returns "7" when passed 7' do
+     expect(fizzbuzz(7)).to eq 7
+   end
+
 end
